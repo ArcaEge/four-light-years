561ab090829d35c4b8aa3c0edd0a9da0af5e605b
ArcaEge <40526225+ArcaEge@users.noreply.github.com>
2020-04-17 17:20:47 +0100

    Added launches section

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import requests
 
 app = Flask(__name__)
 
 imgweb = requests.get('https://api.nasa.gov/planetary/apod?api_key=lHQ3b7vfBGmXtllBqabTiGtuLoEcVgC20gAlnpyF')
 img = imgweb.json()
+launches = requests.get('https://spacelaunchnow.me/api/3.3.0/launch/upcoming/?format=json&limit=5')
+launches = launches.json()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', img=img)
+    return render_template('index.html', img=img, launch=launches)
